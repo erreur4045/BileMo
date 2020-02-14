4d6eae3eea6057f44a7ef0d143c4e780b6813fa5
maxime <maximethi@hotmail.fr>
2020-02-14 18:35:01 -0500

    Unique client{username}

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Serializer\Annotation\MaxDepth;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ * @UniqueEntity("username")
  */
 class Client implements UserInterface
 {
@@ -22,7 +23,7 @@ class Client implements UserInterface
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      * @Groups({"list_clients", "user_details_route"})
      */
     private $username;
